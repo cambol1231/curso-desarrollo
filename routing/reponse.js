63e79d88f6a7ba9c6ae18c2109efce4677dae1e5
Cristian Bolivar <cambol1231@gmail.com>
2020-10-22 22:51:57 -0500

    CROS

--- routing/reponse.js
+++
@@ -1,8 +0,0 @@
-module.exports = ({
-  succes: (res, data, status = 200) => {
-    return res.status(status).json(data)
-  },
-  error: (res, error, status = 400) => {
-    return res.status(status).json({ ...error })
-  }
-})
